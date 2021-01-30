2a473172850466f58fedb6f25073bbec4c8093c0
Facker <2362315840@qq.com>
2021-01-30 16:16:12 +0800

    编写了add_header(),将用户提供的首行内容添加到table_header[]中

--- table.py
+++ table.py
@@ -1,18 +1,48 @@
-from typing import Literal
+# coding=utf-8
+from typing import Literal, List
+from dataclasses import dataclass
+
+
+JustifyMethod = Literal['right', 'left', 'center', 'full']
+
+
+@dataclass()
+class Header:
+    """定义表格的Header属性"""
+
+    content: str = None
+    """表格首行的内容"""
+
+    justify: JustifyMethod = 'full'
+    """内容的间距"""
+
+    width: int = None
+
 
 class Table:
     """用于在控制台显示表格的类"""
-    JustifyMethod = Literal['right', 'left', 'center']
+    table_header: List[Header]
 
     def __init__(self,
-                 title:str=None
+                 title: str = None
                  ):
         self.title = title
-
 
     def add_header(
             self,
             content: str,
-            justify: JustifyMethod="right",
-            width: int=None
-    ):
+            justify: JustifyMethod = "full",
+            width: int = None
+    ) -> None:
+        """将用户提供的每一个表格的首行定义为对象添加到table_header
+
+        :param content: 保存用户提供的表格首行的内容
+        :param justify: 定义显示时，内容是居中还是向右（左）对齐，或者是填充
+        :param width: 定义其行的宽度，但提供的的参数小于的content时，将重新计算宽度
+        """
+        header = Header(
+            content=content,
+            justify=justify,
+            width=width,
+        )
+        self.table_header.append(header)
